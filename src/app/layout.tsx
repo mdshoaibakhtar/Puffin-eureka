96b88bde72272d98d400d8ecc2bbdfa44e1cbf5e
Md Shoaib <mdshoaibakhtar966@gmail.com>
2025-09-23 20:20:02 +0530

    Implement heroui toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Loader from '../components/Generic/Loader';
 import { MyProvider } from './context/MyContext'; // Import your provider
-import {ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,21 +22,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <MyProvider>
-          <ToastContainer
-            position="top-right"
-            autoClose={2000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick={false}
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-          />
-          <Loader />
-          {/* <Webchat /> */}
-          {children}
+          <Providers>
+            {children}
+          </Providers>
         </MyProvider>
       </body>
     </html>
---
+++ src/app/providers.tsx
@@ -0,0 +1,12 @@
+// app/providers.tsx
+
+import {HeroUIProvider, ToastProvider} from '@heroui/react'
+
+export function Providers({children}: { children: React.ReactNode }) {
+  return (
+    <HeroUIProvider>
+    <ToastProvider/>
+      {children}
+    </HeroUIProvider>
+  )
+}
